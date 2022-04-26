0ab0af7aa4653e9cd21d39adb0cbafe709d574aa
TheTzvi <60061254+TheTzvi@users.noreply.github.com>
2022-04-27 00:24:44 +0300

    add to cart now has numbers

--- src/shared/components/Navigation/NavLinks.js
+++ src/shared/components/Navigation/NavLinks.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import CartContext from '../../../store/cart-context';
+
+import React, { useContext } from 'react';
 import { NavLink } from 'react-router-dom';
 import Icon, { ShoppingCartOutlined } from '@ant-design/icons';
 
@@ -6,12 +8,13 @@ import Icon, { ShoppingCartOutlined } from '@ant-design/icons';
 import './NavLinks.css';
 
 const NavLinks = props => {
+    const cart = useContext(CartContext);
     return <ul className="nav-links">
         <li>
             <NavLink to="/about">אודות</NavLink>
         </li>
         <li>
-            <NavLink to="/cart">  <ShoppingCartOutlined /> עגלה    </NavLink>
+            <NavLink id="cartLink" to="/cart">  <ShoppingCartOutlined /> {cart.items.reduce((a, b)=>a = a+b.amount, 0)} עגלה    </NavLink>
         </li>
         <li>
             <NavLink to="/u1/orders">ההזמנות שלי</NavLink>
