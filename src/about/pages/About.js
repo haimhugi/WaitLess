b216d8e8882176377f56ce04a2bf02ebf916a501
TheTzvi <60061254+TheTzvi@users.noreply.github.com>
2022-04-28 00:35:43 +0300

    some visual changes

--- src/about/pages/About.js
+++ src/about/pages/About.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
 import Card from '../../shared/components/UIElements/Card';
+import './About.css'
 
 const About = () => {
     return (
-        <Card>
+        <Card className="about-para">
             <h1>אודות האתר</h1>
             <h3>האתר הוקם כדי להקל על החיים של הסועדים , האתר והרעיון נוצר על ידי שלושה סטודנטים של המכללה למנהל</h3>
             <h3>באמצעות האתר תוכלו להיכנס למסעדה ובלי צורך להמתין זמן רב עד שהמלצר יתפנה תוכלו להזמין את המנות האהובות עליכם ישירות למטבח </h3>
