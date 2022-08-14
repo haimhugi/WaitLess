3987d96b681d6343b65e74a4f20914a4d66bd077
haimhugi <tzvidissen@mobilegroup.co.il>
2022-08-14 12:20:31 +0300

    Add Catagory now works

--- src/category/components/AddCategory.js
+++ src/category/components/AddCategory.js
@@ -2,15 +2,32 @@ import React, { useState } from 'react';
 
 import { Button, Form, Input } from 'antd';
 import "antd/dist/antd.css";
+import { useHttpClient } from '../../shared/hooks/http-hook';
 
 import Modal from '../../shared/components/UIElements/Modal';
 
 const AddCategory = props => {
 
+    const { isLoading, error, sendRequest, clearError } = useHttpClient();
 
-    const onFinish = (values) => {
+    const onFinish = async (values) => {
         console.log('Success:', values.name);
-        //send POST to backend
+        try {
+            await sendRequest(
+                'http://localhost:5001/api/meals/add-category',
+                'POST',
+                JSON.stringify({
+                    name: values.name
+                }),
+                {
+                    'Content-Type': 'application/json'
+                }
+            );
+
+            console.log('Received values of form: ', values);
+        } catch (err) {
+            console.log('add catagory error: ', err);
+         }
         props.onClose();
     };
 
@@ -32,6 +49,7 @@ const AddCategory = props => {
                     remember: true,
                 }}
                 onFinish={onFinish}
+                onSubmit={onFinish}
                 onFinishFailed={onFinishFailed}
                 autoComplete="off"
             >
