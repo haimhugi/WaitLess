caa594ec24d4b89a58751d6cca03c36cae83a527
haimhugi <65680555+haimhugi@users.noreply.github.com>
2022-08-18 11:21:30 +0300

    delete forgot password

--- src/auth/components/Login.js
+++ src/auth/components/Login.js
@@ -144,10 +144,6 @@ const Login = () => {
                         <Form.Item name="remember" valuePropName="checked" noStyle>
                             <Checkbox>Remember me</Checkbox>
                         </Form.Item>
-
-                        <a className="login-form-forgot" href="">
-                            Forgot password
-                        </a>
                     </Form.Item>
 
                     <Form.Item>
