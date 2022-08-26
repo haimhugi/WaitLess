cc9e2d39c9d9eb4ed929df9293452e5292e6f13d
haimhugi <65680555+haimhugi@users.noreply.github.com>
2022-08-26 14:03:35 +0300

    css about

--- src/auth/components/Login.js
+++ src/auth/components/Login.js
@@ -17,121 +17,121 @@ import { useHttpClient } from "../../shared/hooks/http-hook";
 import "./Login.css";
 
 const Login = () => {
-  const CartCtx = useContext(CategoryContext);
-  const history = useHistory();
-  const AuthCtx = useContext(AuthContext);
-  const RegisterCtx = useContext(RegisterContext);
+    const CartCtx = useContext(CategoryContext);
+    const history = useHistory();
+    const AuthCtx = useContext(AuthContext);
+    const RegisterCtx = useContext(RegisterContext);
 
-  const { isLoading, error, sendRequest, clearError } = useHttpClient();
+    const { isLoading, error, sendRequest, clearError } = useHttpClient();
 
-  const onFinish = async (values) => {
-    try {
-      const responseData = await sendRequest(
-        "http://localhost:5001/api/users/login",
-        "POST",
-        JSON.stringify({
-          email: values.username,
-          password: values.password,
-        }),
-        {
-          "Content-Type": "application/json",
+    const onFinish = async (values) => {
+        try {
+            const responseData = await sendRequest(
+                "http://localhost:5001/api/users/login",
+                "POST",
+                JSON.stringify({
+                    email: values.username,
+                    password: values.password,
+                }),
+                {
+                    "Content-Type": "application/json",
+                }
+            );
+            console.log(responseData.user);
+            AuthCtx.changeToLoggedIn(responseData.user.id, responseData.user.isAdmin);
+            CartCtx.changeCategory("הכל");
+            history.push("/meals");
+            console.log("Success");
+        } catch (err) {
+            //add Try Again modal(?)
+            console.log(err);
         }
-      );
-      console.log(responseData.user);
-      AuthCtx.changeToLoggedIn(responseData.user.id, responseData.user.isAdmin);
-      CartCtx.changeCategory("הכל");
-      history.push("/meals");
-      console.log("Success");
-    } catch (err) {
-      //add Try Again modal(?)
-      console.log(err);
-    }
-  };
+    };
 
-  const changeRegisterToTrue = () => {
-    RegisterCtx.changeRegister(true);
-    console.log("changeRegisterToTrue run");
-  };
+    const changeRegisterToTrue = () => {
+        RegisterCtx.changeRegister(true);
+        console.log("changeRegisterToTrue run");
+    };
 
-  return (
-    <React.Fragment>
-      <ErrorModal error={error} onClear={clearError} />
-      <Card className="login-main">
-        {isLoading && <LoadingSpinner asOverlay />}
-        <Form
-          name="normal_login"
-          className="login-form"
-          initialValues={{
-            remember: true,
-          }}
-          onFinish={onFinish}
-        >
-          <Form.Item
-            name="username"
-            rules={[
-              {
-                required: true,
-                message: "Please input your E-Mail!",
-              },
-            ]}
-          >
-            <Input
-              prefix={<UserOutlined className="site-form-item-icon" />}
-              placeholder="E-Mail"
-            />
-          </Form.Item>
-          <Form.Item
-            name="password"
-            rules={[
-              {
-                required: true,
-                message: "Please input your Password!",
-              },
-            ]}
-          >
-            <Input
-              prefix={<LockOutlined className="site-form-item-icon" />}
-              type="password"
-              placeholder="Password"
-            />
-          </Form.Item>
-          <Form.Item>
-            <Form.Item name="remember" valuePropName="checked" noStyle>
-              <Checkbox>Remember me</Checkbox>
-            </Form.Item>
-          </Form.Item>
+    return (
+        <React.Fragment>
+            <ErrorModal error={error} onClear={clearError} />
+            <Card className="login-main">
+                {isLoading && <LoadingSpinner asOverlay />}
+                <Form
+                    name="normal_login"
+                    className="login-form"
+                    initialValues={{
+                        remember: true,
+                    }}
+                    onFinish={onFinish}
+                >
+                    <Form.Item
+                        name="username"
+                        rules={[
+                            {
+                                required: true,
+                                message: "Please input your E-Mail!",
+                            },
+                        ]}
+                    >
+                        <Input
+                            prefix={<UserOutlined className="site-form-item-icon" />}
+                            placeholder="E-Mail"
+                        />
+                    </Form.Item>
+                    <Form.Item
+                        name="password"
+                        rules={[
+                            {
+                                required: true,
+                                message: "Please input your Password!",
+                            },
+                        ]}
+                    >
+                        <Input.Password
+                            prefix={<LockOutlined className="site-form-item-icon" />}
+                            type="password"
+                            placeholder="Password"
+                        />
+                    </Form.Item>
+                    <Form.Item>
+                        <Form.Item name="remember" valuePropName="checked" noStyle>
+                            <Checkbox>Remember me</Checkbox>
+                        </Form.Item>
+                    </Form.Item>
 
-          <Form.Item className="auth-form-btns">
-            <Button
-              type="primary"
-              htmlType="submit"
-              style={{
-                backgroundColor: "black",
-                borderColor: "#676767de",
-                borderRadius: "6px",
-                margin: "3px",
-              }}
-              className="login-form-button"
-            >
-              Log in
-            </Button>
-            <Button
-              style={{
-                backgroundColor: "#e2e2e267",
-                borderColor: "#676767de",
-                color: "white",
-                borderRadius: "6px",
-                margin: "3px",
-              }}
-              onClick={changeRegisterToTrue}
-            >
-              register now!
-            </Button>
-          </Form.Item>
-        </Form>
-      </Card>
-    </React.Fragment>
-  );
+                    <Form.Item className="auth-form-btns">
+                        <Button
+                            type="primary"
+                            htmlType="submit"
+                            style={{
+                                backgroundColor: "black",
+                                borderColor: "#676767de",
+                                borderRadius: "6px",
+                                margin: "3px",
+                            }}
+                            className="login-form-button"
+                        >
+                            Log in
+                        </Button>
+                        <Button
+                            style={{
+                                backgroundColor: "#e2e2e267",
+                                borderColor: "#676767de",
+                                color: "white",
+                                borderRadius: "6px",
+                                margin: "3px",
+                            }}
+                            onClick={changeRegisterToTrue}
+                        >
+                            register now!
+                        </Button>
+                    </Form.Item>
+                </Form>
+            </Card>
+        </React.Fragment>
+    );
 };
 
 export default Login;
